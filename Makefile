a96948acb336930a60230c18bcfeaee11231da81
Antonio Murdaca <runcom@redhat.com>
2016-03-02 16:38:17 +0100

    secretservice: add D-Bus secret service
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
-.PHONY: all deps osxkeychain test validate wincred
+.PHONY: all deps osxkeychain secretservice test validate wincred
+
+TRAVIS_OS_NAME ?= linux
 
 all: test
 
@@ -9,15 +11,29 @@ osxkeychain:
 	mkdir -p bin
 	go build -o bin/docker-credential-osxkeychain osxkeychain/cmd/main_darwin.go
 
-test:
-	# tests all packages except vendor
-	go test -v `go list ./... | grep -v /vendor/`
-
-validate:
-	go vet ./credentials ./osxkeychain
-	golint `go list ./... | grep -v /vendor/`
-	gofmt -s -l `ls **/*.go | grep -v vendor`
+secretservice:
+	mkdir -p bin
+	go build -o bin/docker-credential-secretservice secretservice/cmd/main_linux.go
 
 wincred:
 	mkdir -p bin
 	go build -o bin/docker-credential-wincred wincred/cmd/main_windows.go
+
+test:
+	# tests all packages except vendor
+	go test -v `go list ./... | grep -v /vendor/`
+
+vet: vet_$(TRAVIS_OS_NAME)
+	go vet ./credentials
+
+vet_osx:
+	go vet ./osxkeychain
+
+vet_linux:
+	go vet ./secretservice
+
+validate: vet
+	for p in `go list ./... | grep -v /vendor/`; do \
+		golint $$p ; \
+	done
+	gofmt -s -l `ls **/*.go | grep -v vendor`
---
+++ secretservice/secretservice_linux.go
@@ -0,0 +1,84 @@
+package secretservice
+
+/*
+#cgo pkg-config: libsecret-1
+
+#include "secretservice_linux.h"
+#include <stdlib.h>
+*/
+import "C"
+import (
+	"errors"
+	"unsafe"
+
+	"github.com/docker/docker-credential-helpers/credentials"
+)
+
+type secretservice struct{}
+
+// New creates a new secretservice.
+func New() credentials.Helper {
+	return secretservice{}
+}
+
+// Add adds new credentials to the keychain.
+func (h secretservice) Add(creds *credentials.Credentials) error {
+	if creds == nil {
+		return errors.New("missing credentials")
+	}
+	server := C.CString(creds.ServerURL)
+	defer C.free(unsafe.Pointer(server))
+	username := C.CString(creds.Username)
+	defer C.free(unsafe.Pointer(username))
+	password := C.CString(creds.Password)
+	defer C.free(unsafe.Pointer(password))
+
+	if err := C.add(server, username, password); err != nil {
+		defer C.g_error_free(err)
+		errMsg := (*C.char)(unsafe.Pointer(err.message))
+		return errors.New(C.GoString(errMsg))
+	}
+	return nil
+}
+
+// Delete removes credentials from the keychain.
+func (h secretservice) Delete(serverURL string) error {
+	if serverURL == "" {
+		return errors.New("missing server url")
+	}
+	server := C.CString(serverURL)
+	defer C.free(unsafe.Pointer(server))
+
+	if err := C.delete(server); err != nil {
+		defer C.g_error_free(err)
+		errMsg := (*C.char)(unsafe.Pointer(err.message))
+		return errors.New(C.GoString(errMsg))
+	}
+	return nil
+}
+
+// Get returns the username and password to use for a given registry server URL.
+func (h secretservice) Get(serverURL string) (string, string, error) {
+	if serverURL == "" {
+		return "", "", errors.New("missing server url")
+	}
+	var username *C.char
+	defer C.free(unsafe.Pointer(username))
+	var password *C.char
+	defer C.free(unsafe.Pointer(password))
+	server := C.CString(serverURL)
+	defer C.free(unsafe.Pointer(server))
+
+	err := C.get(server, &username, &password)
+	if err != nil {
+		defer C.g_error_free(err)
+		errMsg := (*C.char)(unsafe.Pointer(err.message))
+		return "", "", errors.New(C.GoString(errMsg))
+	}
+	user := C.GoString(username)
+	pass := C.GoString(password)
+	if pass == "" {
+		return "", "", credentials.ErrCredentialsNotFound
+	}
+	return user, pass, nil
+}
